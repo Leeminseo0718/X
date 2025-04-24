134d8dc0530afea2beb49efba0124e264f3fe337
이민서 <leeminseo2249@gmail.com>
2025-04-24 15:25:55 +0900

    배열을 이용한 POST API 테스트

---
+++ controller/auth.mjs
@@ -0,0 +1,23 @@
+import * as authRepository from "../data/auth.mjs";
+
+export async function signup(req, res, next) {
+  const { userid, password, name, email } = req.body;
+  const users = await authRepository.createUser(userid, password, name, email);
+  if (users) {
+    res.status(201).json(users);
+  } else {
+    res.status(500).json({ message: "회원가입 실패" });
+  }
+}
+
+export async function login(req, res, next) {
+  const { userid, password } = req.body;
+  const user = await authRepository.login(userid, password);
+  if (user) {
+    res.status(200).json({ message: `${userid}님 로그인 성공!`, user });
+  } else {
+    res
+      .status(404)
+      .json({ message: `${userid}님 아이디 또는 비밀번호를 확인하세요.` });
+  }
+}
--- router/auth.mjs
+++ router/auth.mjs
@@ -1,10 +1,16 @@
 import express from "express";
-
+import * as authController from "../controller/auth.mjs";
 const router = express.Router();
 
 //회원가입
+//GET
+router.post("/signup", authController.signup);
 
 //로그인
+//GET
 
+router.post("/login", authController.login);
 //로그인 유지
+//PUT
+
 export default router;
