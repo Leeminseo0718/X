7eff1f4faeb8975c2ce25542ef995bf5f61ddb8b
이민서 <leeminseo2249@gmail.com>
2025-04-28 11:09:07 +0900

    validator 적용

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,18 +1,41 @@
 import express from "express";
 import * as authController from "../controller/auth.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 const router = express.Router();
 
-//회원가입 (post)
+const validateLogin = [
+  body("userid")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("최소 4자이상 입력")
+    .matches(/^[a-zA-Z0-9]*$/)
+    .withMessage("특수문자는 사용불가"),
+  body("password").trim().isLength({ min: 8 }).withMessage("최소 8자이상 입력"),
+  validate,
+];
 
-router.post("/signup", authController.signup);
+const validateSignup = [
+  ...validateLogin,
+  body("name").trim().notEmpty().withMessage("name을 입력"),
+  body("email").trim().isEmail().withMessage("이메일 형식 확인"),
+  validate,
+];
+// 회원가입
+// POST
+// http://127.0.0.1:8080/auth/signup
+router.post("/signup", validateSignup, authController.signup);
 
-//로그아웃 (get)
-router.get("/logout", logout);
+// 로그인
+// POST
+// http://127.0.0.1:8080/auth/login
+router.post("/login", validateLogin, authController.login);
 
-//로그인 (post)
-router.post("/login", authController.login);
+// 로그인 유지
 
-//사용자 정보 확인 (get)
-router.get("/me", me);
 
 export default router;
+
+
+
+
