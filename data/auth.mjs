2f9e6b5c038f1cce81a87185d12a8f19e5b6750d
Leeminseo0718 <jud718@naver.com>
2025-05-12 11:36:37 +0900

    Mongoose 적용

--- data/auth.mjs
+++ data/auth.mjs
@@ -1,32 +1,34 @@
-// import { db } from "../db/database.mjs";
-import MongoDB from "mongodb";
-import { getUsers } from "../db/database.mjs";
-const ObjectID = MongoDB.ObjectId;
+import mongoose from "mongoose";
+import { useVirtualId } from "../db/database.mjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    userid: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    url: String,
+  },
+  { versionKey: false }
+);
+
+useVirtualId(userSchema);
+
+const User = mongoose.model("User", userSchema);
 
 export async function createUser(user) {
-  return getUsers()
-    .insertOne(user)
-    .then((result) => result.insertedId.toString());
-}
-
-export async function login(userid, password) {
-  const user = users.find(
-    (user) => user.userid === userid && user.password === password
-  );
-  return user;
+  return new User(user).save().then((data) => data.id);
 }
 
 export async function findByUserid(userid) {
-  return getUsers().find({ userid }).next().then(mapOptionalUser);
+  return User.findOne({ userid });
 }
 
 export async function findByid(id) {
-  return getUsers()
-    .find({ _id: new ObjectID(id) })
-    .next()
-    .then(mapOptionalUser);
+  return User.findById(id);
 }
 
 function mapOptionalUser(user) {
   return user ? { ...user, id: user._id.toString() } : user;
 }
+
